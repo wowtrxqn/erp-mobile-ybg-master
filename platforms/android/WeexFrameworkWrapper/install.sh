42ed28c11dee2bc8d5c660928dcd2f1cdbb284c3
licailc <1536522740@qq.com>
2018-07-30 16:25:51 +0800

    去除不用的插件

--- platforms/android/WeexFrameworkWrapper/install.sh
+++ platforms/android/WeexFrameworkWrapper/install.sh
@@ -1,44 +1,12 @@
 #!/usr/bin/env bash
+echo "开始更新依赖库"
+rm -r wxframework/
+rm -r sdk/
+rm -r nexus/
 
-echo "执行 git submodule update --init --recursive "
-git submodule update --init --recursive
+git clone https://github.com/bmfe/WeexErosFramework.git "wxframework"
 
-echo "切换主分支"
-# git clone https://github.com/bmfe/WeexErosFramework.git "wxframework"
-
-# git clone https://github.com/bmfe/eros-nexus.git "nexus"
-
-# git clone https://github.com/bmfe/eros-plugin-android-amap.git "ErosPluginAmap"
-
-# git clone https://github.com/bmfe/eros-plugin-android-getui.git "erosplugingt"
-
-# git clone https://github.com/bmfe/eros-plugin-android-wxpay.git "erospluginwxpay"
-
-# git clone https://github.com/bmfe/eros-plugin-android-wxshare.git "erospluginumeng"
-
-cd wxframework
-git checkout master
+git clone https://github.com/bmfe/eros-nexus.git "nexus"
 cd ../
-
-cd nexus
-git checkout master
-cd ../
-
-cd ErosPluginAmap
-git checkout master
-cd ../
-
-cd erosplugingt
-git checkout master
-cd ../
-
-cd erospluginwxpay
-git checkout master
-cd ../
-
-cd erospluginumeng
-git checkout master
-cd ../
-
 
 echo "依赖库更新完成"
